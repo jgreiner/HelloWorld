d7990d43ec255a8db1180be8e37daf6d8259f41f
Jochen Greiner <jochen@greiner.de>
2017-07-11 17:52:36 +0200

    amend

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -33,10 +33,12 @@ class HelloWorldServiceProvider extends ServiceProvider
 
         }, 0);
 
+        /*
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
 
             $partial->set('navigation', 'HelloWorld::content.Navigation');
         }, 0);
+        */
         return false;
     }
 }
