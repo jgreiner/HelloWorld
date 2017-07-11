ea7ef6c8bfbb82b2947e989ecb6fa22230c2c66b
Jochen Greiner <jochen@greiner.de>
2017-07-11 15:33:12 +0200

    theme integration

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace HelloWorld\Providers;
 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 /**
  * Class HelloWorldServiceProvider
@@ -10,11 +14,23 @@ use Plenty\Plugin\ServiceProvider;
 class HelloWorldServiceProvider extends ServiceProvider
 {
 
-	/**
-	 * Register the service provider.
-	 */
-	public function register()
-	{
-		$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        //$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+    }
+
+
+    /**
+     * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
+            $partial->set('footer', 'HelloWorld::content.ThemeFooter');
+        }, 0);
+        return false;
+    }
 }
